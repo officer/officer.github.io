61f7a621ffbafedcc7747f09efdf89b4f41ec4fd
officer <kasaiso@amazon.com>
2019-10-18 22:23:10 +0100

    test

---
+++ precache-manifest.6f9825dec037e741c8321b655305f716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44b18e1d20b94f662e94bc58ed17514e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1730bb12f89c1cfc563",
+    "url": "/static/css/main.131cd185.chunk.css"
+  },
+  {
+    "revision": "8d27042e0e7b1649e505",
+    "url": "/static/js/2.e53c2176.chunk.js"
+  },
+  {
+    "revision": "d1730bb12f89c1cfc563",
+    "url": "/static/js/main.e97bd71b.chunk.js"
+  },
+  {
+    "revision": "7ca0ca60848caa9b1ecd",
+    "url": "/static/js/runtime-main.b98cec83.js"
+  }
+]);
